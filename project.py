afc142770d6d35f01699e243b54839d63b2f99fd
Abdijabar <aymohamed@middlebury.edu>
2017-11-28 19:59:15 -0500

    more elaborative comments, surpressing warnings that come with usage of Jupyter notebook

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
-#Gad, Abdi,Kieran
-
+#Gad, Abdi, Kieran
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
 import os
@@ -7,52 +8,40 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-#Adam --> optimization algorithm that can used instead of tochastic gradient descent
-#to update network weights iterative based in training data.
-from keras.optimizers import Adam
-from keras.layers.normalization import BatchNormalization
-from keras.utils import np_utils
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-
 
 train_data = pd.read_json('data/train.json')
-#the angles of inclination seem to be in degrees. For the sake of
+#the angles of inclination are in degrees (we believe). For the sake of
 #simplicity, for now we replace them with 0
 train_data.inc_angle = train_data.inc_angle.replace('na',0)
+#train_data.head()  #print this to get a sense of the data
 
 
-#print this to see how the angles of inclination are now replaced with all zeros
-train_data.tail()
-
-#here we are getting our training examples and the output respectively 
+#Here we are getting our training examples and the output respectively
 X = train_data.drop(['is_iceberg'], axis=1)
 y = train_data.is_iceberg
 
+#feature scaling the data
 def scale_data(df):
     imgs = []
-    # use for loop inside the list
+    # use for loop inside list, for less code
     for index, row in df.iterrows():
-        # data has two bands
+        # the data has two bands
         band_1 = np.array(row['band_1']).reshape(75,75)
         band_2 = np.array(row['band_2']).reshape(75,75)
+        # we could use sklearn feature scaling libraries, but...
         b_scale_1 = (band_1 - band_1.mean()) / (band_1.max() - band_1.min())
         b_scale_2 = (band_2 - band_2.mean()) / (band_2.max() - band_2.min())
         imgs.append(np.dstack((b_scale_1, b_scale_2)))
     return np.array(imgs)
 
 X = scale_data(X)
-X
 print(X.shape)
 print(y.shape)
 
-
+# this is just, adding for more data
 # using keras in-built function to get more data on training data
 train_keras_more_data = ImageDataGenerator(rotation_range=8, width_shift_range=0.08, shear_range=0.3,
                          height_shift_range=0.08, zoom_range=0.08)
-
 
 #making our model
 model = Sequential()
@@ -102,7 +91,7 @@ save_best_score = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor=
 monitor_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
 
 
-
+#fitting the model
 model.fit(X, y, batch_size=batch_size,
-          epochs=10, verbose=1, callbacks=[earlyStopping,
+          epochs=4 , verbose=1, callbacks=[earlyStopping,
           save_best_score, monitor_loss], validation_split=0.25)
