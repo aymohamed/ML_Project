1e36a99961eebb775eddcb0dcbcc713a8fc22db0
Kieran Parikh <kieran@parikh.ca>
2017-12-04 15:12:54 -0500

    amended model printing

--- project.py
+++ project.py
@@ -7,6 +7,7 @@ import warnings
 warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
+import datetime
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2' #Hide unwanted tf output
 import matplotlib.pyplot as plt
@@ -22,12 +23,13 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.utils import plot_model
 
+
 def loadTrainData():
     CVsetSize = 0.2
     #loading training data, modifying inc angles and extracting training examples and
     #target values from the data
 
-    train_data = pd.read_json('data/train.json')
+    train_data = pd.read_json('Data/train.json')
     # fix entries where inc angle is not a number:
     train_data.inc_angle = train_data.inc_angle.replace('na',0)
     X = train_data.drop(['is_iceberg'], axis=1)
@@ -52,52 +54,54 @@ def scaleData(df):
 
 def createModel():
     model = Sequential()
+    dropoutRate = 0.5
+    print("Dropout Rate: " + str(dropoutRate))
 
     # Input layer
-    model.add(Conv2D(64, kernel_size=(6, 6), input_shape=(75, 75, 2),activation='relu'))
+    model.add(Conv2D(64, kernel_size=(9, 9), input_shape=(75, 75, 2),activation='relu'))
     model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
-    model.add(Dropout(0.2))
+    model.add(Dropout(dropoutRate))
 
     # hidden layer 1
-    model.add(Conv2D(128,  kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(128,  kernel_size=(6, 6), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-    model.add(Dropout(0.4))
+    model.add(Dropout(dropoutRate))
     
     # hidden layer 2
     model.add(Conv2D(128,  kernel_size=(3, 3),activation='relu'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-    model.add(Dropout(0.4))
+    model.add(Dropout(dropoutRate))
     
 
     # hidden layer 3
     model.add(Conv2D(64,  kernel_size=(3, 3),activation='relu'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-    model.add(Dropout(0.2))
+    model.add(Dropout(dropoutRate))
 
     model.add(Flatten())
 
     # Fully connected layer
     model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.4))
+    model.add(Dropout(dropoutRate))
 
     #Dense layer
 
     model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.4))
+    model.add(Dropout(dropoutRate))
 
     # Output layer
     model.add(Dense(1, activation="sigmoid"))
 
     optimizer = Adam(lr=0.001, decay=0.0)
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    model.compile(loss='mean_squared_logarithmic_error', optimizer=optimizer, metrics=['accuracy'])
 
     return model
 
 def trainModel(model, X_train, Y_train, X_CV, Y_CV):
-    batch_size = 64
-    epochs = 9
+    batch_size = 32
+    epochs = 20
 
 
     earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
@@ -105,11 +109,10 @@ def trainModel(model, X_train, Y_train, X_CV, Y_CV):
     monitor_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
 
     # Use Keras data generators on train set
-    dataGen = ImageDataGenerator(rotation_range=8, width_shift_range=0.08, shear_range=0.3,
-                         height_shift_range=0.08, zoom_range=0.08)
+    dataGen = ImageDataGenerator(rotation_range=8, width_shift_range=0.08, shear_range=0.3, height_shift_range=0.08, zoom_range=0.08)
     dataGen.fit(X_train)
 
-    model.fit_generator(dataGen.flow(X_train, Y_train, batch_size=batch_size), steps_per_epoch=len(X_train) / batch_size, epochs=epochs, verbose=1, callbacks=[earlyStopping, save_best_score, monitor_loss], validation_data=(X_CV, Y_CV))
+    model.fit_generator(dataGen.flow(X_train, Y_train, batch_size=batch_size), steps_per_epoch=len(X_train) / batch_size, epochs=epochs, verbose=2, callbacks=[earlyStopping, save_best_score, monitor_loss], validation_data=(X_CV, Y_CV))
 
     return model
 
@@ -124,6 +127,8 @@ def printModel(model):
     model.summary()
 
 def main():
+    now = datetime.datetime.now()
+    print("*** TEST - " + now.strftime("%Y-%m-%d %H:%M") + " ***")
     X_train, Y_train, X_CV, Y_CV = loadTrainData()
     X_train = scaleData(X_train)
     X_CV = scaleData(X_CV)
